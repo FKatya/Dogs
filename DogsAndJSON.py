2956d64a2a68a2a5c8a952157eda6c4f8abd2d08
FKatya <eafedot@gmail.com>
2024-10-22 21:32:50 +0300

    Создана функция загрузки изображения в метку show_image

--- DogsAndJSON.py
+++ DogsAndJSON.py
@@ -2,32 +2,33 @@
 Получаем изображение собачек с сайта https://dog.ceo,
 исользуя API и изучаем формат JSON
 '''
-from tkinter import ttk
 from tkinter import *
+from tkinter import ttk
+from tkinter import messagebox as mb
 from PIL import Image,ImageTk
 import requests
 from io import BytesIO
-#
-#
-#
-# def show_image():
-#     image_url = get_image()
-#     if image_url:
-#         try:
-#             # Получаем картинку в виде бинарного файла по url
-#             # и преобразовываем в изображение и сжимаем до размера
-#             # 640x480
-#             response = requests.get(image_url)
-#             img_data = BytesIO(response.content)
-#             img = Image.open(img_data)
-#             img.thumbnail((640,480))
-#             img=ImageTk.PhotoImage(img)
-#             label.config(image=img)
-#             label.image = img
-#         except Exception as e:
-#             tkinter.messagebox.showerror('Error',f' Не удалось получить изображение: {e}')
-#
-#
+
+
+def show_image():
+    image_url = get_dog_image()
+    if image_url:
+        try:
+            # Получаем картинку в виде бинарного файла по url
+            # и преобразовываем в изображение и сжимаем до размера
+            # 640x480
+            response = requests.get(image_url,stream=True)
+            response.raise_for_status()
+            img_data = BytesIO(response.content)
+            img = Image.open(img_data)
+            img.thumbnail((300,300))
+            img=ImageTk.PhotoImage(img)
+            label.config(image=img)
+            label.image = img
+        except Exception as e:
+            mb.showerror('Ошибка',f' Не удалось получить изображение: {e}')
+
+
 # def get_image():
 #     try:
 #         response = requests.get('https://dog.ceo/api/breeds/image/random')
